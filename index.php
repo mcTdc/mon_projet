d712f6921ea40d59b191ccf34a6ce5016ec9fae2
marie <mariecharlotterenou@icloud.com>
2019-08-29 14:45:26 -0400

    regles de git, test 3

--- index.php
+++ index.php
@@ -1,2 +1,17 @@
-blablabla 
-bojour git
+GitHub - GitLab
+
+ouvrir powershell ou GitBash 
+
+
+pour crer la commande : Git init
+c'est un dossier caché donc il faut dans affichage, selectionné "Extensions de noms de fichiers" 
+et "Elements masqués"
+Mieux vaut toujours copié le dossier parent, celi a la racine car certain dossier caché pourrai 
+ne pas etre copié
+La nature du fichier n'est pas importante
+
+commande git status : donne le statut du dossier git pui git add qui permet d'ajouter le fichier créer au git
+des que l'on fait des modifications importantes : git add
+Pour faire un comit (mettre en ligne la version de son travail, permet de faire des backup) :
+/!\ Il faut absolument documenter son commit A CHAQUE COMMIT sinon ca bug /!\ donc faire git commit -m"blablabla" 
+S'il faut ajouter plusieurs fichiers sur un staging 
