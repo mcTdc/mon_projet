7c8a652563b3965ebac3b03861e35a2e53069d12
marie <mariecharlotterenou@icloud.com>
2019-08-29 14:47:47 -0400

    ajout test 4

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 GitHub - GitLab
 
-ouvrir powershell ou GitBash 
+ouvrir powershell ou GitBash ou Visual Code Studio qui propose des lignes de commandes directement integrés dans le logiciel de code
 
 
 pour crer la commande : Git init
@@ -11,7 +11,9 @@ ne pas etre copié
 La nature du fichier n'est pas importante
 
 commande git status : donne le statut du dossier git pui git add qui permet d'ajouter le fichier créer au git
+
 des que l'on fait des modifications importantes : git add
+
 Pour faire un comit (mettre en ligne la version de son travail, permet de faire des backup) :
 /!\ Il faut absolument documenter son commit A CHAQUE COMMIT sinon ca bug /!\ donc faire git commit -m"blablabla" 
 S'il faut ajouter plusieurs fichiers sur un staging 
